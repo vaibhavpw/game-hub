541a2fdade014adef5137307549762e98f3b8640
vaibhav <you@example.com>
2024-01-06 11:18:06 +0530

    Install chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function App() {
+  
+
+  return (
+    <>
+  <Button colorScheme='blue'>Button</Button>
+  <h1>Hello </h1>
+   
+    </>
+  )
+}
+
+export default App
