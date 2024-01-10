160860ce0c868c983818b5762b514206b0126ce6
vaibhav <you@example.com>
2024-01-10 15:09:32 +0530

    Refactor:Remove duplicated styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,21 +1,32 @@
-
 import apiClient from "../services/api-client";
 import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
-const skeletons=[1,2,3,4,5,6];
+import GameCardContainer from "./GameCardContainer";
+const skeletons = [1, 2, 3, 4, 5, 6];
 
 const GameGrid = () => {
-const {games,error,isLoading}=useGames();
+  const { games, error, isLoading } = useGames();
 
   return (
     <>
-     { error && <Text bg={'red'}>{error}</Text>}
-      <SimpleGrid columns={{sm:1,md:2,lg:3}} spacing={10} padding={'10px'}>
-        {isLoading && skeletons.map(skeleton=> <GameCardSkeleton key={skeleton} />)}
+      {error && <Text bg={"red"}>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3 }}
+        spacing={10}
+        padding={"10px"}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />{" "}
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game}/>
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
