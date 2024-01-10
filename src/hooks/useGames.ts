f6ceaa04707138c0b09660f2ed2d2f2a987eaa69
vaibhav <you@example.com>
2024-01-10 11:37:56 +0530

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,45 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import apiClient from '../services/api-client';
-import { CanceledError } from 'axios';
+import React, { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
-export interface Platform{
-  id:number;
-  name:string;
-  slug:string
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image:string;
-    parent_platforms:{platform:Platform}[]
-
-  }
-  
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-
-  
-const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-  
-    useEffect(() => {
-        const controller=new AbortController();
-      apiClient
-        .get<FetchGamesResponse>("/games")
-        .then((res) => setGames(res.data.results))
-        .catch((err) => {
-            if(err instanceof CanceledError) return ;
-            setError(err.message)})
-     
-        return ()=>controller.abort();
-    },[]);
-  return (
-    {games,error}
-  )
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-export default useGames
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+  return { games, error };
+};
+
+export default useGames;
